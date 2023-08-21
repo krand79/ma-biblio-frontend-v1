c757aa11d5f71173f79379bfe0ff8b105ac58746
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-08-21 06:53:25 +0100

    small update in the footer, typo

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footer footer-center p-4 bg-base-300 text-base-content">
 	<div>
-		<p>Copyright © 2023 - All right reserved by Ma Biblio Ltd, create with ❤️ by ALKADAH Rand</p>
+		<p>Copyright © 2023 - All right reserved by Ma Biblio Ltd, created with ❤️ by ALKADAH Rand</p>
 	</div>
 </footer>
